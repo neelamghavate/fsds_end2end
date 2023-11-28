f86b2db5f19a057e823e427d751f0e1c6f0cac9b
Krish Borhade <ghavateneelam@hotmail.com>
2023-11-28 23:51:47 +0530

    copied files from sunny

--- src/DimondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DimondPricePrediction/pipelines/prediction_pipeline.py
@@ -1,40 +1,36 @@
 import os
 import sys
 import pandas as pd
-from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
+from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.utils.utils import load_object
 
 
 class PredictPipeline:
     def __init__(self):
         pass
-
+    
     def predict(self,features):
         try:
-           preprocessor_path =  os.path.join("artifacts","preprocessor.pkl")
-           model_path = os.path.join("artifacts","model.pkl")
-           logging.info("Preprocessing file and model file collected")
-
-           preprocessor = load_object(preprocessor_path)
-           model = load_object(model_path)
-           logging.info("object loaded")
-
-           scaled_data = preprocessor.transform(features)
-           logging.info("data is scaled")
-
-           pred = model.predict(scaled_data)
-           logging.info("Prediction process done")
-
-           return pred
-
-
-
-
+            preprocessor_path=os.path.join("artifacts","preprocessor.pkl")
+            model_path=os.path.join("artifacts","model.pkl")
+            
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+            
+            scaled_data=preprocessor.transform(features)
+            
+            pred=model.predict(scaled_data)
+            
+            return pred
+            
+            
+        
         except Exception as e:
             raise customexception(e,sys)
-        
-
+    
+    
+    
 class CustomData:
     def __init__(self,
                  carat:float,
@@ -56,8 +52,8 @@ class CustomData:
         self.cut = cut
         self.color = color
         self.clarity = clarity
-
-
+            
+                
     def get_data_as_dataframe(self):
             try:
                 custom_data_input_dict = {
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,15 +1,17 @@
-from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+
+#from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
+
 
 import os
 import sys
-import src.DimondPricePrediction.logger 
-import src.DimondPricePrediction.exception
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
 import pandas as pd
-
-
-
 
 obj=DataIngestion()
 
@@ -22,3 +24,7 @@ train_arr,test_arr=data_transformation.initialize_data_transformation(train_data
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
+
+
+#model_eval_obj = ModelEvaluation()
+#model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
