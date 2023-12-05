38f7cee4594e727146d1200dc8af0665fa325566
Krish Borhade <ghavateneelam@hotmail.com>
2023-12-06 00:37:59 +0530

    MLFLOW and DVC added

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 First end tp end Project
+
+
+#dagshub
+
+MLFLOW_TRACKING_URI=https://dagshub.com/neelamghavate/fsds_end2end.mlflow \
+MLFLOW_TRACKING_USERNAME=neelamghavate \
+MLFLOW_TRACKING_PASSWORD=94b6a0e9195d188bf5da7bee35e9d35b5efb9f9a  \
+python script.py
+
+#Run this to export as env variables:
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/neelamghavate/fsds_end2end.mlflow
+
+export MLFLOW_TRACKING_USERNAME=neelamghavate
+
+export MLFLOW_TRACKING_PASSWORD=94b6a0e9195d188bf5da7bee35e9d35b5efb9f9a
+
+
+##DVC COMMANDS
+
+dvc init
+dvc add D:\ineuron\fsds_end2end\notebooks\data\gemstone.csv
+create file dvc.yaml with code
+dvc.repro
--- src/DimondPricePrediction/components/model_evaluation.py
+++ src/DimondPricePrediction/components/model_evaluation.py
@@ -7,15 +7,19 @@ import mlflow.sklearn
 import numpy as np
 import pickle
 from src.DimondPricePrediction.utils.utils import load_object
+from src.DimondPricePrediction.exception import customexception
+from src.DimondPricePrediction.logger import logging
 
 
 
 class ModelEvaluation:
     def __init__(self):
+        logging.info("inside Model evaluation ")
         pass
 
     
     def eval_metrics(self,actual, pred):
+        logging.info("inside eval_metrics ")
         rmse = np.sqrt(mean_squared_error(actual, pred))# here is RMSE
         mae = mean_absolute_error(actual, pred)# here is MAE
         r2 = r2_score(actual, pred)# here is r3 value
@@ -23,15 +27,15 @@ class ModelEvaluation:
 
 
     def initiate_model_evaluation(self,train_array,test_array):
+        logging.info("nside initiate_model_evaluation")
+
         try:
             X_test,y_test=(test_array[:,:-1], test_array[:,-1])
-
+            
             model_path=os.path.join("artifacts","model.pkl")
             model=load_object(model_path)
 
-        
-
-            mlflow.set_registry_uri("https://dagshub.com/sunny.savita/fsdsmendtoend.mlflow")
+            mlflow.set_registry_uri("https://dagshub.com/neelamghavate/fsds_end2end.mlflow")
             
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
             
@@ -49,7 +53,7 @@ class ModelEvaluation:
                 mlflow.log_metric("r2", r2)
                 mlflow.log_metric("mae", mae)
 
-
+                # this condition is for Dagshub
                 # Model registry does not work with file store
                 if tracking_url_type_store != "file":
 
@@ -59,11 +63,9 @@ class ModelEvaluation:
                     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
                     mlflow.sklearn.log_model(model, "model", registered_model_name="ml_model")
                 else:
+                    #This condition for local
                     mlflow.sklearn.log_model(model, "model")
-
-
-                
-
             
         except Exception as e:
-            raise e
+            #print(e)
+            raise customexception(e,sys)
